dc46a7fed28ec1745857bbd4651e9bd55d73af0d
Afeez <dosunmu.afeez1@gmail.com>
2025-02-08 00:02:52 +0100

    update microservice

---
+++ employee-service/src/main/java/com/example/employee_service/constants/APIConstant.java
@@ -0,0 +1,5 @@
+package com.example.employee_service.constants;
+
+public class APIConstant {
+    public static final String EMPLOYEE_BASE_URL = "/api/v1/employee";
+}
---
+++ user-service/src/main/java/com/example/user_service/constants/APIConstant.java
@@ -0,0 +1,5 @@
+package com.example.user_service.constants;
+
+public class APIConstant {
+    public static final String USER_BASE_URL = "/api/v1/user";
+}
